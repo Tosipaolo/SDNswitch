76b35845d2f01b8313068094854ae08352babf89
Tosipaolo <tosipa99@outlook.it>
2021-05-27 11:37:26 +0200

    aggiunto file senza rest

--- downhandler.py
+++ downhandler.py
@@ -3,7 +3,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, ether_types
+from ryu.lib.packet import packet, ethernet, ether_types, arp
 from ryu.topology.api import get_all_switch, get_all_link, get_all_host
 import ryu.app.ofctl.api as ofctl
 import networkx as nx
@@ -23,8 +23,6 @@ class DOWNHANDLER(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(DOWNHANDLER, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self.arp_table = {}
-        self.sw = {}
         self.SWITCH_LIST = []
         self.LINK_LIST = []
         self.HOST_LIST = []
@@ -152,6 +150,9 @@ class DOWNHANDLER(app_manager.RyuApp):
             print("Cancellazione Flow entries nello switch ", str(datapath.id))
             datapath.send_msg(flow_del)
 
+
+
+
         Dp_Flows = getFlowtable(dp_id)
 
         print('DOWNLOAD FLOWTABLE DEL DATAPATH %s', str(dp_id))
@@ -179,48 +180,71 @@ class DOWNHANDLER(app_manager.RyuApp):
                         print("MATCH! regola cancellata @@@@@@@@@@@@@@@")
 
 
+
+
+    def find_destination_switch(self,destination_mac):
+        for host in get_all_host(self):
+            if host.mac == destination_mac:
+                return (host.port.dpid, host.port.port_no)
+        return (None,None)
+
+    def find_next_hop_to_destination(self, source_id, destination_id):
+        net = nx.DiGraph()
+        for link in get_all_link(self):
+            net.add_edge(link.src.dpid, link.dst.dpid, port=link.src.port_no)
+
+        path = nx.shortest_path(
+            net,
+            source_id,
+            destination_id
+        )
+
+        first_link = net[ path[0] ][ path[1] ]
+
+        return first_link['port']
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-        dpid = datapath.id
-
-        self.SWITCH_LIST = get_all_switch(self)
-        self.LINK_LIST = get_all_link(self)
-        self.HOST_LIST = get_all_host(self)
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-        dst = eth.dst
-        src = eth.src
-        dpid = datapath.id
 
-        assert eth is not None
-
-        dst = eth.dst
-        src = eth.src
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+        # se ARP esegui proxy arp
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            self.proxy_arp(msg)
             return
 
-        self.mac_to_port[dpid][src] = in_port
+        # ignora pacchetti non IPv4 (es. ARP, LLDP)
+        if eth.ethertype != ether_types.ETH_TYPE_IP:
+            return
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+        destination_mac = eth.dst
+
+        # trova switch destinazione
+        (dst_dpid, dst_port) = self.find_destination_switch(destination_mac)
+
+        # host non trovato
+        if dst_dpid is None:
+            # print "DP: ", datapath.id, "Host not found: ", pkt_ip.dst
+            return
+
+        if dst_dpid == datapath.id:
+            # da usare se l'host e' direttamente collegato
+            output_port = dst_port
         else:
-            out_port = ofproto.OFPP_FLOOD
+            # host non direttamente collegato
+            output_port = self.find_next_hop_to_destination(datapath.id, dst_dpid)
 
-        #        self.logger.info("packet in %s %s %s %s %s", dpid, src, dst, in_port, out_port)
+        # print "DP: ", datapath.id, "Host: ", pkt_ip.dst, "Port: ", output_port
 
-        actions = [
-            parser.OFPActionOutput(out_port)
-        ]
-
-        assert msg.buffer_id == ofproto.OFP_NO_BUFFER
-
+        # inoltra il pacchetto corrente
+        actions = [parser.OFPActionOutput(output_port)]
         out = parser.OFPPacketOut(
             datapath=datapath,
             buffer_id=msg.buffer_id,
@@ -230,24 +254,79 @@ class DOWNHANDLER(app_manager.RyuApp):
         )
         datapath.send_msg(out)
 
-        # if the output port is not FLOODING
-        # install a new flow rule *for the next packets*
-        if out_port != ofproto.OFPP_FLOOD:
-            # install a new flow rule
-            match = parser.OFPMatch(
-                eth_src=src,
-                eth_dst=dst
+        # aggiungi la regola
+        match = parser.OFPMatch(
+            eth_dst=destination_mac
+        )
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                [parser.OFPActionOutput(output_port)]
             )
-            inst = [
-                parser.OFPInstructionActions(
-                    ofproto.OFPIT_APPLY_ACTIONS,
-                    actions
-                )
-            ]
-            ofmsg = parser.OFPFlowMod(
-                datapath=datapath,
-                priority=10,
-                match=match,
-                instructions=inst,
-            )
-            datapath.send_msg(ofmsg)
+        ]
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=10,
+            match=match,
+            instructions=inst,
+            buffer_id=msg.buffer_id
+        )
+        datapath.send_msg(mod)
+
+        return
+
+    def proxy_arp(self, msg):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt_in = packet.Packet(msg.data)
+        eth_in = pkt_in.get_protocol(ethernet.ethernet)
+        arp_in = pkt_in.get_protocol(arp.arp)
+
+        # gestiamo solo i pacchetti ARP REQUEST
+        if arp_in.opcode != arp.ARP_REQUEST:
+            return
+
+        destination_host_mac = None
+
+        for host in get_all_host(self):
+            if arp_in.dst_ip in host.ipv4:
+                destination_host_mac = host.mac
+                break
+
+        # host non trovato
+        if destination_host_mac is None:
+            return
+
+        pkt_out = packet.Packet()
+        eth_out = ethernet.ethernet(
+            dst=eth_in.src,
+            src=destination_host_mac,
+            ethertype=ether_types.ETH_TYPE_ARP
+        )
+        arp_out = arp.arp(
+            opcode=arp.ARP_REPLY,
+            src_mac=destination_host_mac,
+            src_ip=arp_in.dst_ip,
+            dst_mac=arp_in.src_mac,
+            dst_ip=arp_in.src_ip
+        )
+        pkt_out.add_protocol(eth_out)
+        pkt_out.add_protocol(arp_out)
+        pkt_out.serialize()
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofproto.OFP_NO_BUFFER,
+            in_port=ofproto.OFPP_CONTROLLER,
+            actions=[parser.OFPActionOutput(in_port)],
+            data=pkt_out.data
+        )
+        datapath.send_msg(out)
+        return
+
+
+
+
